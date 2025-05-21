cf34ac648f42cb71f9e801623b35ce82034b3094
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2025-05-21 15:52:54 +0400

    Feat: Add MaterialUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '@/theme';
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
+import ModeSwitch from '@/components/mode_switch';
+
+
 export const metadata: Metadata = {
   title: "Dashboard - Ti Kloud Réunion",
   description: "Ti Kloud Réunion’s centralized web dashboard application.",
@@ -15,10 +23,18 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="apple-mobile-web-app-title" content="Ti Kloud Réunion" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </head>
       <body>
-        {children}
+        <InitColorSchemeScript attribute="class" />
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <ModeSwitch />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
-    </html>
+    </html >
   );
 }
